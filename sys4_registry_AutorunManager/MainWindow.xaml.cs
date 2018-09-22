7c50ad4afe9da33ca94488577ca39f2169753e9e
Bl0ck <zail@dublin.com>
2018-09-22 22:01:31 +0300

    alpha

--- sys4_registry_AutorunManager/MainWindow.xaml.cs
+++ sys4_registry_AutorunManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,53 @@ namespace sys4_registry_AutorunManager
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		RegistryKey actualRegistryKey;
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+			Loaded += MainWindow_Loaded;
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				actualRegistryKey = Registry.LocalMachine
+					.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+			
+				foreach (var item in actualRegistryKey.GetValueNames())
+				{
+					myList.Items.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		
+		private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Exe Files (.exe)|*.exe|All Files (*.*)|*.*";
+			if(openFileDialog.ShowDialog() == true)
+			{
+				string shortFileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+				actualRegistryKey.SetValue(shortFileName, $"\"{openFileDialog.FileName}\"");
+				myList.Items.Add(shortFileName);
+			}
+		}
+
+		private void removeBtn_Click(object sender, RoutedEventArgs e)
+		{
+			if(myList.SelectedItems.Count>0)
+			{
+				actualRegistryKey.DeleteValue((string)myList.SelectedItem);
+				myList.Items.Remove(myList.SelectedItem);
+			}
 		}
 	}
 }
